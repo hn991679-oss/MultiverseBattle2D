28078f01a3174d7a452856119e52e302a5e45aa7
hn991679-oss <hn991679@gmail.com>
2026-08-02 17:39:32 +0700

    Create player.js

---
+++ js/player.js
@@ -0,0 +1,83 @@
+// ===========================
+// PLAYER SYSTEM
+// ===========================
+
+const player = {
+
+    x:150,
+
+    y:350,
+
+    speed:6,
+
+    jump:false,
+
+    attack:false,
+
+    skill:false,
+
+    direction:1
+
+};
+
+function moveLeft(){
+
+    player.x-=player.speed;
+
+    player.direction=-1;
+
+    drawPlayer();
+
+}
+
+function moveRight(){
+
+    player.x+=player.speed;
+
+    player.direction=1;
+
+    drawPlayer();
+
+}
+
+function jump(){
+
+    if(player.jump) return;
+
+    player.jump=true;
+
+    player.y-=120;
+
+    drawPlayer();
+
+    setTimeout(()=>{
+
+        player.y=350;
+
+        player.jump=false;
+
+        drawPlayer();
+
+    },400);
+
+}
+
+function drawPlayer(){
+
+    const p=document.querySelector(".player");
+
+    p.style.left=player.x+"px";
+
+    p.style.bottom=player.y+"px";
+
+    if(player.direction==-1){
+
+        p.style.transform="scaleX(-1)";
+
+    }else{
+
+        p.style.transform="scaleX(1)";
+
+    }
+
+}
