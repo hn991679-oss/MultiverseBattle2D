47f65aefe9d0fdc4623f700888aa9276004027a4
hn991679-oss <hn991679@gmail.com>
2026-08-02 17:42:44 +0700

    Create characterData.js

---
+++ js/characterData.js
@@ -0,0 +1,95 @@
+// =========================
+// CHARACTER DATABASE
+// =========================
+
+const characters = {
+
+Rick:{
+    hp:100,
+    atk:10,
+    speed:6,
+    sprite:"assets/rick/normal.png",
+    prime:"assets/rick/prime.png"
+},
+
+Morty:{
+    hp:100,
+    atk:8,
+    speed:7,
+    sprite:"assets/morty/normal.png",
+    evil:"assets/morty/evil.png"
+},
+
+Gojo:{
+    hp:120,
+    atk:12,
+    skill:"Infinity"
+},
+
+Goku:{
+    hp:140,
+    atk:14,
+    skill:"Kamehameha"
+},
+
+Naruto:{
+    hp:130,
+    atk:12,
+    skill:"Rasengan"
+},
+
+Sasuke:{
+    hp:130,
+    atk:13,
+    skill:"Chidori"
+},
+
+Luffy:{
+    hp:135,
+    atk:13,
+    skill:"Gear 5"
+},
+
+Ichigo:{
+    hp:140,
+    atk:14,
+    skill:"Getsuga Tenshou"
+},
+
+Vegeta:{
+    hp:145,
+    atk:15,
+    skill:"Final Flash"
+},
+
+Madara:{
+    hp:160,
+    atk:16,
+    skill:"Susanoo"
+},
+
+Sukuna:{
+    hp:170,
+    atk:17,
+    skill:"Domain Expansion"
+},
+
+Tanjiro:{
+    hp:120,
+    atk:11,
+    skill:"Hinokami Kagura"
+},
+
+Levi:{
+    hp:110,
+    atk:10,
+    skill:"Spin Attack"
+},
+
+Yuta:{
+    hp:150,
+    atk:15,
+    skill:"Rika"
+}
+
+};
